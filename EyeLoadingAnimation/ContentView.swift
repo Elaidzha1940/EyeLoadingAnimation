4abff10a3dc86e88037c8e9faa823c0378711631
Elaidzha1940 <eli080293@gmail.com>
2023-10-16 02:03:55 +0300

    Done

--- EyeLoadingAnimation/ContentView.swift
+++ EyeLoadingAnimation/ContentView.swift
@@ -15,10 +15,7 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-          
-            
-            HStack {
-                
+            HStack(spacing: 20) {
                 BrowShape()
                     .stroke(Color.white, lineWidth: 3)
                     .frame(width: 160, height: 40)
@@ -28,47 +25,51 @@ struct ContentView: View {
                     .frame(width: 160, height: 40)
             }
             .padding(.bottom, 30)
+            
+            HStack(spacing: 30) {
                 
-                HStack(spacing: 30) {
-                   
-                    Capsule()
-                        .foregroundColor(.white)
-                        .frame(width: 140, height: isAnimating ? 50 : 50)
-                        .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
-                        .overlay(
-                            ZStack {
-                                Circle()
-                                    .stroke(lineWidth: 3)
-                                    .frame(width: 30, height: 30)
-                                    .foregroundColor(.black)
-                                Circle()
-                                    .frame(width: 15, height: 15)
-                                    .foregroundColor(.black)
-                            }
-                        )
-                  
-                    Capsule()
-                        .foregroundColor(.white)
-                        .frame(width: 140, height: isAnimating ? 25 : 50)
-                        .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
-                        .overlay(
-                            ZStack {
-                                Circle()
-                                    .stroke(lineWidth: 3)
-                                    .frame(width: 30, height: 30)
-                                    .foregroundColor(.black)
-                                Circle()
-                                    .frame(width: 15, height: 15)
-                                    .foregroundColor(.black)
-                            }
-                        )
-                }
-                .onAppear {
-                    isAnimating = true
-                }
+                Capsule()
+                    .foregroundColor(.white)
+                    .frame(width: 140, height: isAnimating ? 50 : 50)
+                    .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
+                    .overlay(
+                        ZStack {
+                            Circle()
+                                .stroke(lineWidth: 3)
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                            Circle()
+                                .frame(width: 15, height: 15)
+                                .foregroundColor(.black)
+                        }
+                    )
+                
+                Capsule()
+                    .foregroundColor(.white)
+                    .frame(width: 140, height: isAnimating ? 5 : 50)
+                    .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
+                    .overlay(
+                        ZStack {
+                            Circle()
+                                .stroke(lineWidth: 3)
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                            Circle()
+                                .frame(width: 15, height: 15)
+                                .foregroundColor(.black)
+                        }
+                    )
             }
+            .onAppear {
+                isAnimating = true
+            }
+            
+            NoseShape()
+                .fill(Color.white)
+                .frame(width: 150, height: 50)
+                .offset(y: 40)
+        }
         .preferredColorScheme(.dark)
-        
     }
 }
 
@@ -83,6 +84,16 @@ struct BrowShape: Shape {
         path.addLine(to: CGPoint(x: rect.minX + rect.width * 0.25, y: rect.minY - rect.height * 0.3))
         path.addLine(to: CGPoint(x: rect.minX + rect.width * 0.75, y: rect.minY - rect.height * 0.3))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        return path
+    }
+}
+
+struct NoseShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.midY + 15), control: CGPoint(x: rect.midX - 15, y: rect.midY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.minY), control: CGPoint(x: rect.midX + 15, y: rect.midY))
         return path
     }
 }
