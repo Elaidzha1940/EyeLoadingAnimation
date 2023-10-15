97dda930c0b38b11035851ce9f93b332f8159836
Elaidzha1940 <eli080293@gmail.com>
2023-10-15 21:07:48 +0300

    Add animation

--- EyeLoadingAnimation/ContentView.swift
+++ EyeLoadingAnimation/ContentView.swift
@@ -15,35 +15,74 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Color.black
-                .edgesIgnoringSafeArea(.all)
+          
             
-            HStack(spacing: 30) {
-                Capsule()
-                    .foregroundColor(.white)
-                    .frame(width: 140, height: isAnimating ? 0 : 50)
-                    .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
-                    .overlay(
-                        Circle()
-                            .frame(width: 20, height: 20)
-                    )
+            HStack {
                 
-                Capsule()
-                    .foregroundColor(.white)
-                    .frame(width: 140, height: isAnimating ? 0 : 50)
-                    .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
-                    .overlay(
-                        Circle()
-                            .frame(width: 20, height: 20)
-                    )
+                BrowShape()
+                    .stroke(Color.white, lineWidth: 3)
+                    .frame(width: 160, height: 40)
+                
+                BrowShape()
+                    .stroke(Color.white, lineWidth: 3)
+                    .frame(width: 160, height: 40)
             }
-            .onAppear {
-                isAnimating = true
+            .padding(.bottom, 30)
+                
+                HStack(spacing: 30) {
+                   
+                    Capsule()
+                        .foregroundColor(.white)
+                        .frame(width: 140, height: isAnimating ? 50 : 50)
+                        .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
+                        .overlay(
+                            ZStack {
+                                Circle()
+                                    .stroke(lineWidth: 3)
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(.black)
+                                Circle()
+                                    .frame(width: 15, height: 15)
+                                    .foregroundColor(.black)
+                            }
+                        )
+                  
+                    Capsule()
+                        .foregroundColor(.white)
+                        .frame(width: 140, height: isAnimating ? 25 : 50)
+                        .animation(.easeInOut(duration: 1.2).repeatForever(), value: isAnimating)
+                        .overlay(
+                            ZStack {
+                                Circle()
+                                    .stroke(lineWidth: 3)
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(.black)
+                                Circle()
+                                    .frame(width: 15, height: 15)
+                                    .foregroundColor(.black)
+                            }
+                        )
+                }
+                .onAppear {
+                    isAnimating = true
+                }
             }
-        }
+        .preferredColorScheme(.dark)
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct BrowShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX + rect.width * 0.25, y: rect.minY - rect.height * 0.3))
+        path.addLine(to: CGPoint(x: rect.minX + rect.width * 0.75, y: rect.minY - rect.height * 0.3))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        return path
+    }
 }
