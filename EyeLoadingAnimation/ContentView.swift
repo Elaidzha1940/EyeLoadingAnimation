7d20119c569363546124613a27d8b7eb63b71727
Elaidzha1940 <eli080293@gmail.com>
2023-10-15 20:26:43 +0300

    EyeLoadingAnimation

---
+++ EyeLoadingAnimation/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: EyeLoadingAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EyeLoadingAnimation/EyeLoadingAnimationApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: EyeLoadingAnimation
+//  File: EyeLoadingAnimationApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct EyeLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
